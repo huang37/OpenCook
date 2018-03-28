dc2e63a931ae959c7ac0536fdffb27116fdb2ae2
Huang <huang37@mail2000.com.tw>
2018-03-28 23:18:59 +0800

    first commit

---
+++ pyvenv36-django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/huangxiaopei/Desktop/python101/OpenCook/pyvenv36-django/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
